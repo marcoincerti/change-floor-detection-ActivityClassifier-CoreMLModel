146245a52d38c67321a63aebb7e98d3cd4b89e35
marcoincerti <56672035+marcoincerti@users.noreply.github.com>
2022-08-21 23:58:14 +0200

    try something new

--- change-floor/change-floor/ContentView.swift
+++ change-floor/change-floor/ContentView.swift
@@ -16,10 +16,9 @@ struct ModelConstants {
 }
 
 
-struct ContentView: View {
-    let activityClassificationModel: MyActivityClassifier_10 = try! MyActivityClassifier_10(configuration: .init())
-    @State var currentIndexInPredictionWindow = 0
-    @State var labelPredict = ""
+class ModelPrediction{
+    let activityClassificationModel: MyActivityClassifier4 = try! MyActivityClassifier4(configuration: .init())
+    public var currentIndexInPredictionWindow: Int = 0
     
     let accelDataX = try! MLMultiArray(shape: [ModelConstants.predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
     let accelDataY = try! MLMultiArray(shape: [ModelConstants.predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
@@ -33,49 +32,57 @@ struct ContentView: View {
     let attitudeDataRoll = try! MLMultiArray(shape: [ModelConstants.predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
     let attitudeDataYaw = try! MLMultiArray(shape: [ModelConstants.predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
     
-    @State var stateOutput = try! MLMultiArray(shape:[ModelConstants.stateInLength as NSNumber], dataType: MLMultiArrayDataType.double)
-    let motionManager = CMMotionManager()
+    var stateOutput = try! MLMultiArray(shape:[ModelConstants.stateInLength as NSNumber], dataType: MLMultiArrayDataType.double)
     
     
-    func performModelPrediction () -> String? {
+    func performModelPrediction () -> [String : Double]? {
         // Perform model prediction
         let modelPrediction = try! activityClassificationModel.prediction(acceleration_x: accelDataX, acceleration_y: accelDataY, acceleration_z: accelDataZ, attitude_pitch: attitudeDataPitch, attitude_roll: attitudeDataRoll, attitude_yaw: attitudeDataYaw, rotationRate_x: gyroDataX, rotationRate_y: gyroDataY, rotationRate_z: gyroDataZ, stateIn: stateOutput)
-
+        
         // Update the state vector
         stateOutput = modelPrediction.stateOut
-
+        print(modelPrediction.labelProbability)
         // Return the predicted activity - the activity with the highest probability
-        return modelPrediction.label
+        return modelPrediction.labelProbability
     }
     
-    func addSampleToDataArray (accelSample: CMAccelerometerData, gyroSample: CMGyroData, attSample: CMDeviceMotion) {
+    func addSampleToDataArray (dataMotion: CMDeviceMotion) -> [String : Double]? {
         // Add the current accelerometer reading to the data array
-        accelDataX[[currentIndexInPredictionWindow] as [NSNumber]] = accelSample.acceleration.x as NSNumber
-        accelDataY[[currentIndexInPredictionWindow] as [NSNumber]] = accelSample.acceleration.y as NSNumber
-        accelDataZ[[currentIndexInPredictionWindow] as [NSNumber]] = accelSample.acceleration.z as NSNumber
+        print(dataMotion.userAcceleration)
+        accelDataX[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.userAcceleration.x as NSNumber
+        accelDataY[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.userAcceleration.y as NSNumber
+        accelDataZ[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.userAcceleration.z as NSNumber
         
-        gyroDataX[[currentIndexInPredictionWindow] as [NSNumber]] = gyroSample.rotationRate.x as NSNumber
-        gyroDataY[[currentIndexInPredictionWindow] as [NSNumber]] = gyroSample.rotationRate.y as NSNumber
-        gyroDataZ[[currentIndexInPredictionWindow] as [NSNumber]] = gyroSample.rotationRate.z as NSNumber
+        gyroDataX[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.rotationRate.x as NSNumber
+        gyroDataY[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.rotationRate.y as NSNumber
+        gyroDataZ[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.rotationRate.z as NSNumber
         
-        attitudeDataPitch[[currentIndexInPredictionWindow] as [NSNumber]] = attSample.attitude.pitch as NSNumber
-        attitudeDataRoll[[currentIndexInPredictionWindow] as [NSNumber]] = attSample.attitude.roll as NSNumber
-        attitudeDataYaw[[currentIndexInPredictionWindow] as [NSNumber]] = attSample.attitude.yaw as NSNumber
-
+        //print(dataMotion.attitude)
+        attitudeDataPitch[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.attitude.pitch as NSNumber
+        attitudeDataRoll[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.attitude.roll as NSNumber
+        attitudeDataYaw[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.attitude.yaw as NSNumber
+        
         // Update the index in the prediction window data array
-        currentIndexInPredictionWindow += 1
-
+        currentIndexInPredictionWindow = currentIndexInPredictionWindow + 1
+        
         // If the data array is full, call the prediction method to get a new model prediction.
         // We assume here for simplicity that the Gyro data was added to the data arrays as well.
-        if (currentIndexInPredictionWindow == ModelConstants.predictionWindowSize) {
-            if let predictedActivity = performModelPrediction() {
-
-                labelPredict = predictedActivity
-                // Start a new prediction window
-                currentIndexInPredictionWindow = 0
-            }
-        }
+//        if (currentIndexInPredictionWindow == ModelConstants.predictionWindowSize) {
+//            if let predictedActivity = performModelPrediction() {
+//
+//                currentIndexInPredictionWindow = 0
+//                return predictedActivity
+//            }
+//        }
+        return nil
     }
+    
+}
+
+struct ContentView: View {
+    let modelPredict = ModelPrediction()
+    let motionManager = CMMotionManager()
+    @State var label: [String : Double] = ["mhhh":100]
     
     func startSensors() {
         guard motionManager.isAccelerometerAvailable, motionManager.isGyroAvailable, motionManager.isDeviceMotionAvailable else { return }
@@ -84,34 +91,30 @@ struct ContentView: View {
         motionManager.gyroUpdateInterval = TimeInterval(ModelConstants.sensorsUpdateInterval)
         motionManager.deviceMotionUpdateInterval = TimeInterval(ModelConstants.sensorsUpdateInterval)
         
-        var dataAcc:CMAccelerometerData = CMAccelerometerData()
-        var dataGyro:CMGyroData = CMGyroData()
-        var dataAtt:CMDeviceMotion = CMDeviceMotion()
-        
-        
-        motionManager.startAccelerometerUpdates(to: .main) { accelerometerData, error in
-            guard let accelerometerData = accelerometerData else { return }
-            dataAcc = accelerometerData
+        motionManager.startDeviceMotionUpdates(using: .xArbitraryZVertical, to: .main){ dataMotion, error in
+            guard let dataMotion = dataMotion else { return }
+            guard let data = modelPredict.addSampleToDataArray(dataMotion: dataMotion) else { return }
+            label = data
+            print(label)
         }
-        
-        motionManager.startGyroUpdates(to: .main) { gyroData, error in
-            guard let gyroData = gyroData else { return }
-            dataGyro = gyroData
-        }
-        
-        motionManager.startDeviceMotionUpdates(using: .xMagneticNorthZVertical, to: .main){ attitudineData, error in
-            guard let attitudineData = attitudineData else { return }
-            dataAtt = attitudineData
-        }
-        
-        
-        // Add the current data sample to the data array
-        self.addSampleToDataArray(accelSample: dataAcc, gyroSample: dataGyro, attSample: dataAtt)
     }
     
     var body: some View {
-        Text(labelPredict)
-            .padding()
+        VStack{
+            List {
+                ForEach(label.sorted(by: >), id: \.key) { key, value in
+                    Section(header: Text(key)) {
+                        Text(String(value))
+                    }
+                }
+            }
+            
+            Button(action: {
+                self.startSensors()
+            }) {
+                Text("Go")
+            }
+        }
     }
 }
 
