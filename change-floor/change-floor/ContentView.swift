20c0f8f9038b0d7549896d29f9c59601c0410b9a
marcoincerti <56672035+marcoincerti@users.noreply.github.com>
2022-08-22 17:18:00 +0200

    I calculated the deviation of pressure and relative height for each record in order to normalize the data

--- change-floor/change-floor/ContentView.swift
+++ change-floor/change-floor/ContentView.swift
@@ -17,7 +17,7 @@ struct ModelConstants {
 
 
 class ModelPrediction{
-    let activityClassificationModel: MyActivityClassifier4 = try! MyActivityClassifier4(configuration: .init())
+    let activityClassificationModel: MyActivityClassifier_9 = try! MyActivityClassifier_9(configuration: .init())
     public var currentIndexInPredictionWindow: Int = 0
     
     let accelDataX = try! MLMultiArray(shape: [ModelConstants.predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
@@ -28,16 +28,15 @@ class ModelPrediction{
     let gyroDataY = try! MLMultiArray(shape: [ModelConstants.predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
     let gyroDataZ = try! MLMultiArray(shape: [ModelConstants.predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
     
-    let attitudeDataPitch = try! MLMultiArray(shape: [ModelConstants.predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
-    let attitudeDataRoll = try! MLMultiArray(shape: [ModelConstants.predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
-    let attitudeDataYaw = try! MLMultiArray(shape: [ModelConstants.predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
+    let altitudeRelativeData = try! MLMultiArray(shape: [ModelConstants.predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
+    let pressureData = try! MLMultiArray(shape: [ModelConstants.predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
     
     var stateOutput = try! MLMultiArray(shape:[ModelConstants.stateInLength as NSNumber], dataType: MLMultiArrayDataType.double)
     
     
     func performModelPrediction () -> [String : Double]? {
         // Perform model prediction
-        let modelPrediction = try! activityClassificationModel.prediction(acceleration_x: accelDataX, acceleration_y: accelDataY, acceleration_z: accelDataZ, attitude_pitch: attitudeDataPitch, attitude_roll: attitudeDataRoll, attitude_yaw: attitudeDataYaw, rotationRate_x: gyroDataX, rotationRate_y: gyroDataY, rotationRate_z: gyroDataZ, stateIn: stateOutput)
+        let modelPrediction = try! activityClassificationModel.prediction(acceleration_x: accelDataX, acceleration_y: accelDataY, acceleration_z: accelDataZ, altitude_pressure: pressureData, relativeAltitude: altitudeRelativeData, rotationRate_x: gyroDataX, rotationRate_y: gyroDataY, rotationRate_z:gyroDataZ, stateIn: stateOutput)
         
         // Update the state vector
         stateOutput = modelPrediction.stateOut
@@ -46,9 +45,8 @@ class ModelPrediction{
         return modelPrediction.labelProbability
     }
     
-    func addSampleToDataArray (dataMotion: CMDeviceMotion) -> [String : Double]? {
+    func addSampleToDataArray (dataMotion: CMDeviceMotion, relativeAltitude: Double, pressure: Double) -> [String : Double]? {
         // Add the current accelerometer reading to the data array
-        print(dataMotion.userAcceleration)
         accelDataX[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.userAcceleration.x as NSNumber
         accelDataY[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.userAcceleration.y as NSNumber
         accelDataZ[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.userAcceleration.z as NSNumber
@@ -57,23 +55,22 @@ class ModelPrediction{
         gyroDataY[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.rotationRate.y as NSNumber
         gyroDataZ[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.rotationRate.z as NSNumber
         
-        //print(dataMotion.attitude)
-        attitudeDataPitch[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.attitude.pitch as NSNumber
-        attitudeDataRoll[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.attitude.roll as NSNumber
-        attitudeDataYaw[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.attitude.yaw as NSNumber
+        altitudeRelativeData[[currentIndexInPredictionWindow] as [NSNumber]] = relativeAltitude as NSNumber
+        pressureData[[currentIndexInPredictionWindow] as [NSNumber]] = pressure as NSNumber
+        
         
         // Update the index in the prediction window data array
         currentIndexInPredictionWindow = currentIndexInPredictionWindow + 1
         
         // If the data array is full, call the prediction method to get a new model prediction.
         // We assume here for simplicity that the Gyro data was added to the data arrays as well.
-//        if (currentIndexInPredictionWindow == ModelConstants.predictionWindowSize) {
-//            if let predictedActivity = performModelPrediction() {
-//
-//                currentIndexInPredictionWindow = 0
-//                return predictedActivity
-//            }
-//        }
+        if (currentIndexInPredictionWindow == ModelConstants.predictionWindowSize) {
+            if let predictedActivity = performModelPrediction() {
+
+                currentIndexInPredictionWindow = 0
+                return predictedActivity
+            }
+        }
         return nil
     }
     
@@ -82,21 +79,50 @@ class ModelPrediction{
 struct ContentView: View {
     let modelPredict = ModelPrediction()
     let motionManager = CMMotionManager()
+    let altimeter = CMAltimeter()
+    @State var relativealtitude: Double = 0.0
+    @State var pressure: Double = 0.0
+    @State var prev_pressure: Double = 0.0
+    @State var firstTime = true
+    
     @State var label: [String : Double] = ["mhhh":100]
     
     func startSensors() {
-        guard motionManager.isAccelerometerAvailable, motionManager.isGyroAvailable, motionManager.isDeviceMotionAvailable else { return }
+        guard motionManager.isAccelerometerAvailable, motionManager.isGyroAvailable, motionManager.isDeviceMotionAvailable, CMAltimeter.isRelativeAltitudeAvailable() else { return }
         
         motionManager.accelerometerUpdateInterval = TimeInterval(ModelConstants.sensorsUpdateInterval)
         motionManager.gyroUpdateInterval = TimeInterval(ModelConstants.sensorsUpdateInterval)
         motionManager.deviceMotionUpdateInterval = TimeInterval(ModelConstants.sensorsUpdateInterval)
         
-        motionManager.startDeviceMotionUpdates(using: .xArbitraryZVertical, to: .main){ dataMotion, error in
+        altimeter.startRelativeAltitudeUpdates(to: .main){ dataAltimeter, error in
+            guard let dataAltimeter = dataAltimeter else {  return }
+            
+            let tmp_altitude = dataAltimeter.relativeAltitude as! Double
+            let tmp_pressure = dataAltimeter.pressure as! Double
+            
+            if tmp_altitude - relativealtitude != 0{
+                relativealtitude = tmp_altitude - relativealtitude
+            }
+            
+            if firstTime {
+                prev_pressure = tmp_pressure
+                firstTime = false
+            }
+            
+            if tmp_pressure - prev_pressure != 0{
+                pressure = tmp_pressure - prev_pressure
+                prev_pressure = tmp_pressure
+            }
+        }
+        
+        motionManager.startDeviceMotionUpdates(to: .main){ dataMotion, error in
             guard let dataMotion = dataMotion else { return }
-            guard let data = modelPredict.addSampleToDataArray(dataMotion: dataMotion) else { return }
+            print(relativealtitude, pressure)
+            guard let data = modelPredict.addSampleToDataArray(dataMotion: dataMotion, relativeAltitude: relativealtitude, pressure: pressure) else { return }
             label = data
             print(label)
         }
+        
     }
     
     var body: some View {
