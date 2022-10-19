f525c90d9ded7c6bcdd27ef1cf690ca10e612c05
marcoincerti <56672035+marcoincerti@users.noreply.github.com>
2022-10-19 21:44:08 +0200

    updated UI

--- change-floor/change-floor/ContentView.swift
+++ change-floor/change-floor/ContentView.swift
@@ -17,7 +17,7 @@ struct ModelConstants {
 
 
 class ModelPrediction{
-    let activityClassificationModel: changeFloorClassifier27 = try! changeFloorClassifier27(configuration: .init())
+    let activityClassificationModel: changeFloorClassifier30 = try! changeFloorClassifier30(configuration: .init())
     public var currentIndexInPredictionWindow: Int = 0
     
     let accelDataX = try! MLMultiArray(shape: [ModelConstants.predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
@@ -36,9 +36,9 @@ class ModelPrediction{
     var stateOutput = try! MLMultiArray(shape:[ModelConstants.stateInLength as NSNumber], dataType: MLMultiArrayDataType.double)
     
     
-    func performModelPrediction () -> changeFloorClassifier27Output? {
+    func performModelPrediction () -> changeFloorClassifier30Output? {
         // Perform model prediction
-        let modelPrediction = try! activityClassificationModel.prediction(acceleration_x: accelDataX, acceleration_y: accelDataY, acceleration_z: accelDataZ, differenceAltitude: differenceAltitudeRelativeData, differencePressure: differencePressureData, relativeAltitude: altitudeRelativeData, rotationRate_x:gyroDataX,rotationRate_y: gyroDataY, rotationRate_z: gyroDataZ, stateIn: stateOutput)
+        let modelPrediction = try! activityClassificationModel.prediction(acceleration_x: accelDataX, acceleration_y: accelDataY, acceleration_z: accelDataZ, differenceAltitude: differenceAltitudeRelativeData, differencePressure: differencePressureData, rotationRate_x:gyroDataX,rotationRate_y: gyroDataY, rotationRate_z: gyroDataZ, stateIn: stateOutput)
         
         // Update the state vector
         stateOutput = modelPrediction.stateOut
@@ -46,7 +46,7 @@ class ModelPrediction{
         return modelPrediction
     }
     
-    func addSampleToDataArray (dataMotion: CMDeviceMotion, relativealtitude: Double, diffRelativealtitude: Double, diff_pressure: Double) -> changeFloorClassifier27Output? {
+    func addSampleToDataArray (dataMotion: CMDeviceMotion, relativealtitude: Double, diffRelativealtitude: Double, diff_pressure: Double) -> changeFloorClassifier30Output? {
         // Add the current accelerometer reading to the data array
         accelDataX[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.userAcceleration.x as NSNumber
         accelDataY[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.userAcceleration.y as NSNumber
@@ -56,7 +56,7 @@ class ModelPrediction{
         gyroDataY[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.rotationRate.y as NSNumber
         gyroDataZ[[currentIndexInPredictionWindow] as [NSNumber]] = dataMotion.rotationRate.z as NSNumber
         
-        altitudeRelativeData[[currentIndexInPredictionWindow] as [NSNumber]] = relativealtitude as NSNumber
+        //altitudeRelativeData[[currentIndexInPredictionWindow] as [NSNumber]] = relativealtitude as NSNumber
         differenceAltitudeRelativeData[[currentIndexInPredictionWindow] as [NSNumber]] = diffRelativealtitude as NSNumber
         differencePressureData[[currentIndexInPredictionWindow] as [NSNumber]] = diff_pressure as NSNumber
         //pressureData[[currentIndexInPredictionWindow] as [NSNumber]] = pressure as NSNumber
@@ -69,7 +69,7 @@ class ModelPrediction{
         // We assume here for simplicity that the Gyro data was added to the data arrays as well.
         if (currentIndexInPredictionWindow == ModelConstants.predictionWindowSize) {
             if let predictedActivity = performModelPrediction() {
-
+                
                 currentIndexInPredictionWindow = 0
                 return predictedActivity
             }
@@ -83,7 +83,7 @@ struct ContentView: View {
     let modelPredict = ModelPrediction()
     let motionManager = CMMotionManager()
     let altimeter = CMAltimeter()
-    let height_in_meters = 0.2
+    let height_in_meters = 3.4
     @State var scarto_relative_height = 0.0
     @State var diff_relative_altitude: Double = 0.0
     @State var diff_pressure: Double = 0.0
@@ -100,15 +100,17 @@ struct ContentView: View {
     @State var floor : Int = 0
     
     func check_floor() {
-        if abs(relative_altitude) > height_in_meters - (height_in_meters * 0.2){
-            print(relative_altitude, scarto_relative_height)
+        let relative_altitude_tmp = relative_altitude - scarto_relative_height
+        if abs(relative_altitude_tmp) > height_in_meters /*+ (height_in_meters * 0.15)*/{
+            print(relative_altitude_tmp, scarto_relative_height)
             label_changed = "Hai cambiato piano usando \(label_predict). "
-            if relative_altitude > 0 {
+            if relative_altitude_tmp > 0 {
                 floor = floor + 1
             }else {
                 floor = floor - 1
             }
-            scarto_relative_height = scarto_relative_height + relative_altitude
+            scarto_relative_height = scarto_relative_height + relative_altitude_tmp
+            //modelPredict.currentIndexInPredictionWindow = 0
             //relative_altitude = relative_altitude - scarto_relative_height
         }
     }
@@ -116,13 +118,13 @@ struct ContentView: View {
     func startSensors() {
         guard motionManager.isAccelerometerAvailable, motionManager.isGyroAvailable, motionManager.isDeviceMotionAvailable, CMAltimeter.isRelativeAltitudeAvailable() else { return }
         
-        motionManager.accelerometerUpdateInterval = TimeInterval(ModelConstants.sensorsUpdateInterval)
-        motionManager.gyroUpdateInterval = TimeInterval(ModelConstants.sensorsUpdateInterval)
+        //motionManager.accelerometerUpdateInterval = TimeInterval(ModelConstants.sensorsUpdateInterval)
+        //motionManager.gyroUpdateInterval = TimeInterval(ModelConstants.sensorsUpdateInterval)
         motionManager.deviceMotionUpdateInterval = TimeInterval(ModelConstants.sensorsUpdateInterval)
         
         altimeter.startRelativeAltitudeUpdates(to: .main){ dataAltimeter, error in
             guard let dataAltimeter = dataAltimeter else {  return }
-            relative_altitude = (dataAltimeter.relativeAltitude as! Double) - scarto_relative_height
+            relative_altitude = (dataAltimeter.relativeAltitude as! Double)
             let pressure = dataAltimeter.pressure as! Double
             
             if firstTime{
@@ -144,7 +146,6 @@ struct ContentView: View {
         
         motionManager.startDeviceMotionUpdates(to: .main){ dataMotion, error in
             guard let dataMotion = dataMotion else { return }
-            //print(dataMotion.userAcceleration.x, dataMotion.userAcceleration.y, dataMotion.userAcceleration.z)
             guard let data = modelPredict.addSampleToDataArray(dataMotion: dataMotion, relativealtitude: relative_altitude, diffRelativealtitude: diff_relative_altitude, diff_pressure: diff_pressure) else { return }
             dict_prob = data.labelProbability
             label_predict = data.label
@@ -157,28 +158,40 @@ struct ContentView: View {
         altimeter.stopRelativeAltitudeUpdates()
         modelPredict.currentIndexInPredictionWindow = 0
         dict_prob = ["mhhh":100]
-        label_predict = "mhhh"
+        label_predict = "I'm thinking"
     }
     
     var body: some View {
         VStack{
+            Spacer()
             Text(String(label_predict))
+                .font(.largeTitle)
+                .bold()
                 .padding()
-            Text(label_changed + "Sei al piano: \(floor)")
+            Text("You're on the floor: \(floor)")
                 .padding()
                 .onReceive(timer) { time in
-                                check_floor()
-                            }
-            Text("\(relative_altitude)")
-                .padding()
-            List {
-                ForEach(dict_prob.sorted(by: >), id: \.key) { key, value in
-                    Section(header: Text(key)) {
-                        Text(String(value))
-                    }
+                    check_floor()
                 }
-            }
-            
+            //            Text("\(relative_altitude)")
+            //                .padding()
+            //            List {
+            //                ForEach(dict_prob.sorted(by: >), id: \.key) { key, value in
+            //                    Section(header: Text(key)) {
+            //                        Text(String(value))
+            //                    }
+            //                }
+            //            }
+            Spacer()
+            ZStack {
+                // 2
+                CircularProgressView(progress: ((relative_altitude - scarto_relative_height)/height_in_meters))
+                // 3
+                Text("\(relative_altitude - scarto_relative_height)")
+                    .font(.largeTitle)
+                    .bold()
+            }.frame(width: 300, height: 300)
+            Spacer()
             Button(action: {
                 if sensor_enable{
                     self.stopSensor()
@@ -193,7 +206,8 @@ struct ContentView: View {
                 }else{
                     Text("Go")
                 }
-            }
+            }.buttonStyle(.borderedProminent)
+            Spacer()
         }
     }
 }
@@ -201,5 +215,46 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CircularProgressView: View {
+    let progress: Double
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(
+                    Color.pink.opacity(0.5),
+                    lineWidth: 50
+                )
+            if progress >= 0{
+                Circle()
+                    .trim(from: 0, to: progress)
+                    .stroke(
+                        Color.pink,
+                        style: StrokeStyle(
+                            lineWidth: 30,
+                            lineCap: .round
+                        )
+                    )
+                    .rotationEffect(.degrees(-90))
+                // 1
+                    .animation(.easeOut, value: progress)
+            } else {
+                Circle()
+                    .trim(from: 0, to: abs(progress))
+                    .stroke(
+                        Color.purple,
+                        style: StrokeStyle(
+                            lineWidth: 30,
+                            lineCap: .round
+                        )
+                    )
+                    .rotationEffect(.degrees(-90))
+                // 1
+                    .animation(.easeOut, value: progress)
+            }
+        }
     }
 }
